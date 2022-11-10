685b2871eab0c6ddb111f6740bfe87953bcafe1e
SwithJonlamonthon <64071398+SwithJonlamonthon@users.noreply.github.com>
2022-11-10 07:51:15 +0000

    Update lab 4

--- main.py
+++ main.py
@@ -254,17 +254,84 @@ x = pyramid.cal(17,7,10)
 print("𝑣𝑜𝑙𝑢𝑚𝑒 𝑜𝑓 𝑝𝑦𝑟𝑎𝑚𝑖𝑑 :",x)
 
 
+class Node:
+     def __init__(self, data, next = None, previous = None):
+        self.data = data;
+        self.next = next;
+        self.previous = previous
+
 class li:## For init list
-  def __init__(self,value,next=None,prev=None):
-    self.value = value 
-    self.next = next
-    self.prev = prev
-  def create(self,data):
-    if self.value == None:
-      self.value = data
-    else:
-      self.next = data
-      self.prev = self.next
+    def __init__(self):
+        self.head = None
+     
+    def insertAtStart(self, data):
+        if self.head == None:
+            newNode = Node(data)
+            self.head = newNode
+        else:
+            newNode = Node(data)
+            self.head.previous = newNode
+            newNode.next = self.head
+            self.head = newNode
+            
+    # for inserting at end of linked list
+    def insertAtEnd(self, data):
+        newNode = Node(data)
+        temp = self.head
+        while(temp.next != None):
+            temp = temp.next
+        temp.next = newNode
+        newNode.previous = temp
+        
+    # deleting a node from linked list
+    def delete(self, data):
+        temp = self.head
+        if(temp.next != None):
+            # if head node is to be deleted
+            if(temp.data == data):
+                temp.next.previous = None
+                self.head = temp.next
+                temp.next = None
+                return
+            else:
+                while(temp.next != None):
+                    if(temp.data == data):
+                        break
+                    temp = temp.next
+                if(temp.next):
+                    # if element to be deleted is in between
+                    temp.previous.next = temp.next
+                    temp.next.previous = temp.previous
+                    temp.next = None
+                    temp.previous = None
+                else:
+                    # if element to be deleted is the last element
+                    temp.previous.next = None
+                    temp.previous = None
+                return
+        
+        if (temp == None):
+            return
+        
+    # for printing the contents of linked lists
+    def printdll(self):
+        temp = self.head
+        while(temp != None):
+            print(temp.data, end=' ')
+            temp = temp.next
+        
+     
+            
+            
+dll = li()
+dll.insertAtStart(1)
+dll.insertAtStart(2)
+dll.insertAtEnd(3)
+dll.insertAtStart(4)
+dll.printdll()
+dll.delete(2)
+print()
+dll.printdll()
         
 
 
